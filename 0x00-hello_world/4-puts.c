0d063a9ccf1c4f3a1289be03a9487538be205041
tarek <tahdeeb@yahoo.com>
2023-03-16 09:31:31 -0700

    task four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-
+/*
+ * main
+ * prints a statement followed by a new line
+ * return 0
+ */
 int main(void)
 
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
