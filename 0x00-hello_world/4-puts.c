8f29208f9574d9d67b08b6048a7a3191960ddf6a
tarek <tahdeeb@yahoo.com>
2023-03-16 11:08:53 -0700

    finished

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-/*
- * main - Prints "Programming is like buildidng a multillingual 
- * puzzle, followed by a new line."
- * Return: Always 0.
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 
