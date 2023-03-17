73382f568f0e2dd0fa23ed63c2c520ddc974c5c0
tarek <tahdeeb@yahoo.com>
2023-03-17 10:13:25 -0700

    task two

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char l;
+	for (l = 'a'; l <= 'z'; l++)
+	putchar(l);
+	putchar('\n');
+	return (0);
+}
