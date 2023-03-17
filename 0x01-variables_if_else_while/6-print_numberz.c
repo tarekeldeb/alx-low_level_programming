3e17c793ecc6ba41f703a1882264a31a66dcc247
tarek <tahdeeb@yahoo.com>
2023-03-17 12:40:10 -0700

    check3

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int d;
 
-	for (d = '0'; d < 10; d++)
+	for (d = 48; d < 58; d++)
 	putchar(d);
 	putchar('\n');
 
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,10 +10,10 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n < 10; n++)
+	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
-		if (n != 9)
+		if (n != 57)
 		{
 			putchar(',');
 			putchar(' ');
