c69a89358f6d8fa5575374b741344a3165ec0d62
tarek <tahdeeb@yahoo.com>
2023-03-23 09:07:55 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,13 +8,14 @@
 
 void print_most_numbers(void)
 {
-	int c;
+	char c;
 
-	for (c = 48; c <= 58; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (!(c == 50 || c == 52))
+		if (!(c == '2' || c == '4'))
 			_putchar(c);
 	}
 	_putchar('\n');
 }
+
 
