7fa04cce3f21ba23159aa93585f75782b081bd90
tarek <tahdeeb@yahoo.com>
2023-03-23 08:32:55 -0700

    task zero

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * a function that checks for uppercase 
+ * upper: return 1
+ * lower: return 0
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
