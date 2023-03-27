d1e4ed7f6ef53e543651a17ba6ee61bccbc4d8c5
tarek <tahdeeb@yahoo.com>
2023-03-27 16:49:18 -0700

    task four

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
+
+void print_rev(char *s)
+{
+	int length = 0;
+	int i;
+    
+	while (s[length] != '\0') 
+	{
+	    length++;
+	}
+    
+	for (i = length - 1; i >= 0; i--) 
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
