3537d2ab2410cb53270816795e1b41cbcf64b885
tarek <tahdeeb@yahoo.com>
2023-03-27 17:48:22 -0700

    check

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,14 +10,13 @@ void print_rev(char *s)
 {
 	int length = 0;
 	int i;
-    
+
 	while (s[length] != '\0') 
 	{
-	    length++;
-	}
-    
+		length++;
+	}	
 	for (i = length - 1; i >= 0; i--) 
-	{
+	{	
 		_putchar(s[i]);
 	}
 	_putchar('\n');
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -14,7 +14,6 @@ void puts2(char *str)
 	{
 		len++;
 	}
-    
 	for (i = 0; i < len; i += 2)
 	{
 		_putchar(*(str + i));
