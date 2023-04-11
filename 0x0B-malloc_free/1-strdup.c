ac2753d1f584c65d89ad411652894a9280935122
tarek <tahdeeb@yahoo.com>
2023-04-10 22:08:03 -0700

    task one

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -4,36 +4,33 @@
 /**
  * _strdup - create a duplicate of a string
  * @str: pointer to the string to be duplicated
- *
- * Return: pointer to the newly allocated duplicate string, or NULL if str is NULL
+ * Return: pointer to the newly allocated duplicate string,
+ * or NULL if str is NULL
  */
 char *_strdup(char *str)
 {
-    int len = 0;
-    int i;
-    char *cpstr;
+	int len = 0;
+	int i;
+	char *cpstr;
 
-    if (str == NULL)
-    {
-        return NULL;
-    }
+	if (str == NULL)
+	{
+	return (NULL);
+	}
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	cpstr = malloc((len + 1) * sizeof(char));
+	if (cpstr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len; i++)
+	{
+		cpstr[i] = str[i];
+	}
+	cpstr[len] = '\0';
 
-    while (str[len] != '\0')
-    {
-        len++;
-    }
-
-    cpstr = malloc((len + 1) * sizeof(char));
-    if (cpstr == NULL)
-    {
-        return NULL;
-    }
-
-    for (i = 0; i < len; i++)
-    {
-        cpstr[i] = str[i];
-    }
-    cpstr[len] = '\0';
-
-    return cpstr;
+	return (cpstr);
 }
