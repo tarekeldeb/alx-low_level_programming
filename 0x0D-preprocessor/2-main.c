d2ee1438c1762aa68d1a57229eb68f247d2c006e
tarek <tahdeeb@yahoo.com>
2023-04-18 14:00:26 -0700

    second DDL

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,27 +1,13 @@
-#include <stdlib.h>
+#include <stdio.h>
+
 /**
  * main - prints the name of the file
  *
  * Return: Always 0 (Success)
  */
-
-int main (void)
+int main(void)
 {
-	char *file = __FILE__;
-	char *ptr = file;
-
-	while (*p != '\0')
-		p++;
-	while (*p != '/' && *p != file)
-		p--;
-	if (*p == '/')
-		p++;
-	while (*p != '/0')
-	{
-		_putchar(*p);
-		p++;
-	}
-	_putchar('\n');
-	retuen (0);
-	
+	printf("%s\n", __FILE__);
+	return (0);
 }
+
