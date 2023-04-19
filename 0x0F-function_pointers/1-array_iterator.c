a4c0369974448d6e24c5a419585c9c945f053243
tarek <tahdeeb@yahoo.com>
2023-04-19 09:56:21 -0700

    check

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,5 @@
-#include <stdlib.h>
 #include "function_pointers.h"
+#include <stdio.h>
 /**
  * array_iterator - prints each array elem on a newl
  * @array: array
@@ -11,8 +11,12 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
+	if (array == NULL || action == NULL)
+		return;
+
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
 	}
 }
+
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include "function_pointers.h"
 /**
  * int_index - return index place if comparison = true, else -1
@@ -9,14 +8,16 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	 int i;
+	int i;
 
-	if (size <= 0)
+	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
-	for (i = 0; i <  size; i++)
+
+	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
 			return (i);
 	}
 	return (-1);
 }
+
