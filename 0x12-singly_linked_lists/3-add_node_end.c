31b8a6e703c03da4863b0a289df72b42a27df796
tarek <tahdeeb@yahoo.com>
2023-04-27 17:01:43 -0700

    check third updates

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -21,8 +21,8 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->next = NULL;
 	if (*head == NULL)
 	{
-		*head = new;
-		return (new);
+		*head = new_node;
+		return (new_node);
 	}
 	
 	while (temp->next)
